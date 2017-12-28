17637b170a39ff781a7e62590d50a6715fc4bfd4
Lexxus <altmoc@gmail.com>
2017-12-28 18:57:50 +0200

    Fix name of domHelper mixin in index export

--- src/mixins/index.js
+++ src/mixins/index.js
@@ -1,6 +1,6 @@
 import ajax from './ajax'
 import pointer from './typeAheadPointer'
 import pointerScroll from './pointerScroll'
-import offset from './domHelpers'
+import domHelpers from './domHelpers'
 
-export default { ajax, pointer, pointerScroll, offset }
+export default { ajax, pointer, pointerScroll, domHelpers }
