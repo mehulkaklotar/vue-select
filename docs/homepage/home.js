b862761cbf9d617b9012d711adcaf4417ecae111
Jeff <jeff.sagal@briteweb.com>
2018-01-13 17:00:50 -0800

    more homepage styling

--- docs/homepage/home.js
+++ docs/homepage/home.js
@@ -7,5 +7,46 @@ Vue.component('v-select', vSelect);
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app'
+  el: '#app',
+  data() {
+    return {
+      loading: false,
+      options: [
+        {
+          title: 'Read the Docs',
+          icon: 'octicon-book',
+          url: 'https://codeclimate.com/github/sagalbot/vue-select'
+        },
+        {
+          title: 'View on GitHub',
+          icon: 'octicon-mark-github',
+          url: 'https://codeclimate.com/github/sagalbot/vue-select'
+        },
+        {
+          title: 'View on NPM',
+          icon: 'octicon-database',
+          url: 'https://codeclimate.com/github/sagalbot/vue-select'
+        },
+        {
+          title: 'View Code Climate Analysis',
+          icon: 'octicon-graph',
+          url: 'https://codeclimate.com/github/sagalbot/vue-select'
+        },
+        {
+          title: 'View Codepen Examples',
+          icon: 'octicon-pencil',
+          url: 'https://codeclimate.com/github/sagalbot/vue-select'
+        },
+      ]
+    }
+  },
+  methods: {
+    onInput(option) {
+      this.loading = true;
+      this.go(option.url);
+    },
+    go(url) {
+      window.location = url;
+    }
+  }
 });
